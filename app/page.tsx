a4c0b45ea1db39b8f223db8c945e2a4ab25c4e5a
Priya Ranjan <priya.ranjan@metyis.com>
2026-06-25 13:14:28 +0530

    Removing-PricingSection-fix

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
       <WhyChooseUs />
       <ClientsSection />
       <CaseStudies />
-      <PricingSection />
+      {/* <PricingSection /> */}
       <ContactSection />
       <FooterSection />
     </>
